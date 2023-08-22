77138e73d2531fc2067fe021baeadd3e27e68f32
Ryan <kochy@hotmail.co.uk>
2023-08-22 12:56:12 +0100

    added link for header

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -1,13 +1,13 @@
 import "../globals.css";
 import Image from "next/image";
+import Link from "next/link";
 import headerBackground from "../../../public/sandboxChessImage.png";
 
 const Header = () => {
     return (
         <div className="flex flex-col items-center mt-10 mb-20">
-            <Image src={headerBackground} alt="Sandbox Chess " />
+            <Link href="/"><Image src={headerBackground} alt="Sandbox Chess" /></Link>
         </div>
-
     )
 }
 
