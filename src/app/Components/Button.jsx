b35be76d41cd314a020660c068f71f7cc4830bc2
Ryan <kochy@hotmail.co.uk>
2023-08-22 12:50:26 +0100

    fixed text only using Link

--- src/app/Components/Button.jsx
+++ src/app/Components/Button.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 const Menu = () => {
     return (
         <div className="flex flex-col items-center">
-            <button className="bg-white shadow-lg hover:bg-green-400 text-black py-2 px-4 rounded-full text-black w-80 my-5 active:scale-y-75 transition-transform"><Link href="/sandbox">Sandbox</Link></button>
-            <button className="bg-white shadow-lg hover:bg-green-400 text-black py-2 px-4 rounded-full text-black w-80 my-5 active:scale-y-75 transition-transform"><Link href="/analysis">Analysis</Link></button>
-            <button className="bg-white shadow-lg hover:bg-green-400 text-black py-2 px-4 rounded-full text-black w-80 my-5 active:scale-y-75 transition-transform"><Link href="/daily_puzzle">Daily Puzzle</Link></button>
+            <Link href="/sandbox"><button className="bg-white shadow-lg hover:bg-green-400 text-black py-2 px-4 rounded-full text-black w-80 my-5 active:scale-y-75 transition-transform">Sandbox</button></Link>
+            <Link href="/analysis"><button className="bg-white shadow-lg hover:bg-green-400 text-black py-2 px-4 rounded-full text-black w-80 my-5 active:scale-y-75 transition-transform">Analysis</button></Link>
+            <Link href="/daily_puzzle"><button className="bg-white shadow-lg hover:bg-green-400 text-black py-2 px-4 rounded-full text-black w-80 my-5 active:scale-y-75 transition-transform">Daily Puzzle</button></Link>
         </div>
 
     )
