a6957cc67aea586b29b4cfbfd27c22296038adf6
Ryan <kochy@hotmail.co.uk>
2023-09-28 14:31:14 +0100

    created mock files for audio and images

---
+++ audioLoader.js
@@ -0,0 +1,15 @@
+export function loadMoveSound() {
+  return require("./public/sounds/move.mp3");
+}
+
+export function loadCaptureSound() {
+  return require("./public/sounds/capture.mp3");
+}
+
+export function loadCastleSound() {
+  return require("./public/sounds/castle.mp3");
+}
+
+export function loadCheckSound() {
+  return require("./public/sounds/check.mp3");
+}
