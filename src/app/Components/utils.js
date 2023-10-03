749f952db1f83e2d2e26eb0368a7a57a56fc6ff1
Ryan <kochy@hotmail.co.uk>
2023-10-03 11:33:52 +0100

    created kingMovement util function

--- src/app/Components/utils.js
+++ src/app/Components/utils.js
@@ -602,6 +602,164 @@ const checkQueenMovement = (startTile, endTile, boardState, colour) => {
   return true;
 };
 
+const checkKingMovement = (startTile, endTile, boardState, colour) => {
+  const columnRef = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  const movement = [];
+
+  if (
+    columnRef.indexOf(startTile.column) < columnRef.indexOf(endTile.column) &&
+    startTile.row < endTile.row
+  ) {
+    let offset = 1;
+    for (let i = 0; i < endTile.row - startTile.row; i++) {
+      movement.push(
+        boardState[startTile.row + i][
+          columnRef.indexOf(startTile.column) + offset
+        ]
+      );
+      offset++;
+    }
+  } else if (
+    columnRef.indexOf(startTile.column) < columnRef.indexOf(endTile.column) &&
+    startTile.row > endTile.row
+  ) {
+    let offset = 2;
+    for (let i = 0; i < startTile.row - endTile.row; i++) {
+      movement.push(
+        boardState[startTile.row - offset][
+          columnRef.indexOf(startTile.column) + 1 + i
+        ]
+      );
+      offset++;
+    }
+  } else if (
+    columnRef.indexOf(startTile.column) > columnRef.indexOf(endTile.column) &&
+    startTile.row < endTile.row
+  ) {
+    let offset = 1;
+    for (
+      let i = 0;
+      i <
+      columnRef.indexOf(startTile.column) - columnRef.indexOf(endTile.column);
+      i++
+    ) {
+      movement.push(
+        boardState[startTile.row + i][
+          columnRef.indexOf(startTile.column) - offset
+        ]
+      );
+      offset++;
+    }
+  } else if (
+    columnRef.indexOf(startTile.column) > columnRef.indexOf(endTile.column) &&
+    startTile.row > endTile.row
+  ) {
+    let offset = 1;
+    for (
+      let i = 0;
+      i <
+      columnRef.indexOf(startTile.column) - columnRef.indexOf(endTile.column);
+      i++
+    ) {
+      movement.push(
+        boardState[startTile.row - 1 - offset][
+          columnRef.indexOf(startTile.column) - offset
+        ]
+      );
+      offset++;
+    }
+  }
+
+  // if moving in straight lines
+  if (
+    endTile.row === startTile.row &&
+    columnRef.indexOf(endTile.column) < columnRef.indexOf(startTile.column)
+  ) {
+    for (
+      let i = 0;
+      i <
+      columnRef.indexOf(startTile.column) - columnRef.indexOf(endTile.column);
+      i++
+    ) {
+      const rowOffset = 1;
+      const columnOffsett = 1;
+      movement.push(
+        boardState[endTile.row - rowOffset][
+          columnRef.indexOf(startTile.column) - columnOffsett - i
+        ]
+      );
+    }
+  } else if (
+    endTile.row === startTile.row &&
+    columnRef.indexOf(endTile.column) > columnRef.indexOf(startTile.column)
+  ) {
+    for (
+      let i = 0;
+      i <
+      columnRef.indexOf(endTile.column) - columnRef.indexOf(startTile.column);
+      i++
+    ) {
+      const rowOffset = 1;
+      const columnOffsett = 1;
+      movement.push(
+        boardState[endTile.row - rowOffset][
+          columnRef.indexOf(startTile.column) + columnOffsett + i
+        ]
+      );
+    }
+  } else if (
+    endTile.row < startTile.row &&
+    columnRef.indexOf(endTile.column) === columnRef.indexOf(startTile.column)
+  ) {
+    for (let i = 0; i < startTile.row - endTile.row; i++) {
+      const rowOffset = 2;
+      movement.push(
+        boardState[startTile.row - rowOffset - i][
+          columnRef.indexOf(startTile.column)
+        ]
+      );
+    }
+  } else if (
+    endTile.row > startTile.row &&
+    columnRef.indexOf(endTile.column) === columnRef.indexOf(startTile.column)
+  ) {
+    for (let i = 0; i < endTile.row - startTile.row; i++) {
+      movement.push(
+        boardState[startTile.row + i][columnRef.indexOf(startTile.column)]
+      );
+    }
+  }
+
+  //removing the endtile
+  const inBetweenTiles = movement.slice(0, -1);
+
+  //all tiles inbetween
+  const inBetweenTilesAreNull = inBetweenTiles.every((piece) => {
+    return piece === null;
+  });
+
+  // checks jumping oposite colours
+  if (!inBetweenTilesAreNull) {
+    return false;
+  }
+
+  if (movement.length > 1) {
+    return false;
+  }
+
+  for (let i = 0; i < movement.length; i++) {
+    if (movement[i] !== null) {
+      if (movement[i].colour === colour) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+
+  return true;
+};
+
 module.exports = {
   checkPawnMovement,
   checkRookMovement,
