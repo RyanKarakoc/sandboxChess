4b3aff431825cc4b9f25be83d1d797dbf51aa289
Ryan <kochy@hotmail.co.uk>
2023-10-24 17:23:15 +0100

    moved bishop functions into own file

--- src/app/Components/utils.js
+++ src/app/Components/utils.js
@@ -92,129 +92,6 @@ const checkKnightMovement = (startTile, endTile, boardState, colour) => {
   }
 };
 
-const checkBishopMovement = (startTile, endTile, boardState, colour) => {
-  const columnRef = ["a", "b", "c", "d", "e", "f", "g", "h"];
-  const diagonal = [];
-
-  if (startTile.row === endTile.row) {
-    return false;
-  }
-
-  if (
-    columnRef.indexOf(startTile.column) === columnRef.indexOf(endTile.column)
-  ) {
-    return false;
-  }
-
-  // creating the diagonal move
-  if (
-    // x pos y pos
-    columnRef.indexOf(startTile.column) < columnRef.indexOf(endTile.column) &&
-    startTile.row < endTile.row &&
-    endTile.row - startTile.row ===
-      columnRef.indexOf(endTile.column) - columnRef.indexOf(startTile.column)
-  ) {
-    console.log("x pos, y pos");
-
-    let offset = 1;
-    for (let i = 0; i < endTile.row - startTile.row; i++) {
-      diagonal.push(
-        boardState[startTile.row + i][
-          columnRef.indexOf(startTile.column) + offset
-        ]
-      );
-      offset++;
-    }
-  } else if (
-    //x pos y neg
-    columnRef.indexOf(startTile.column) < columnRef.indexOf(endTile.column) &&
-    startTile.row > endTile.row &&
-    startTile.row - endTile.row ===
-      columnRef.indexOf(endTile.column) - columnRef.indexOf(startTile.column)
-  ) {
-    console.log("x pos, y neg");
-    let offset = 2;
-    for (let i = 0; i < startTile.row - endTile.row; i++) {
-      diagonal.push(
-        boardState[startTile.row - offset][
-          columnRef.indexOf(startTile.column) + 1 + i
-        ]
-      );
-      offset++;
-    }
-  } else if (
-    // x neg y pos
-    columnRef.indexOf(startTile.column) > columnRef.indexOf(endTile.column) &&
-    startTile.row < endTile.row &&
-    endTile.row - startTile.row ===
-      columnRef.indexOf(startTile.column) - columnRef.indexOf(endTile.column)
-  ) {
-    console.log("x neg, y pos");
-    let offset = 1;
-    for (
-      let i = 0;
-      i <
-      columnRef.indexOf(startTile.column) - columnRef.indexOf(endTile.column);
-      i++
-    ) {
-      diagonal.push(
-        boardState[startTile.row + i][
-          columnRef.indexOf(startTile.column) - offset
-        ]
-      );
-      offset++;
-    }
-  } else if (
-    // x neg y neg
-    columnRef.indexOf(startTile.column) > columnRef.indexOf(endTile.column) &&
-    startTile.row > endTile.row &&
-    startTile.row - endTile.row ===
-      columnRef.indexOf(startTile.column) - columnRef.indexOf(endTile.column)
-  ) {
-    console.log("x neg, y neg");
-    let offset = 1;
-    for (
-      let i = 0;
-      i <
-      columnRef.indexOf(startTile.column) - columnRef.indexOf(endTile.column);
-      i++
-    ) {
-      diagonal.push(
-        boardState[startTile.row - 1 - offset][
-          columnRef.indexOf(startTile.column) - offset
-        ]
-      );
-      offset++;
-    }
-  }
-
-  //removing the endtile
-  const inBetweenTiles = diagonal.slice(0, -1);
-  //all tiles inbetween
-  const inBetweenTilesAreNull = inBetweenTiles.every((piece) => {
-    return piece === null;
-  });
-  // checks jumping oposite colours
-  if (!inBetweenTilesAreNull) {
-    return false;
-  }
-
-  if (diagonal.length === 0) {
-    return false;
-  }
-
-  for (let i = 0; i < diagonal.length; i++) {
-    if (diagonal[i] !== null) {
-      if (diagonal[i].colour === colour) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-  }
-  return true;
-};
-
 const checkQueenMovement = (startTile, endTile, boardState, colour) => {
   const columnRef = ["a", "b", "c", "d", "e", "f", "g", "h"];
   const movement = [];
@@ -546,7 +423,6 @@ const checkKingMovement = (startTile, endTile, boardState, colour) => {
 
 module.exports = {
   checkKnightMovement,
-  checkBishopMovement,
   checkQueenMovement,
   checkKingMovement,
 };
