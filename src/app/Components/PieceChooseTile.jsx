93854d8f0bb453c4436bc6322c40eafb8292d636
RyanKarakoc <kochy@hotmail.co.uk>
2024-02-14 06:56:09 +0000

    added styling

--- src/app/Components/PieceChooseTile.jsx
+++ src/app/Components/PieceChooseTile.jsx
@@ -7,7 +7,7 @@ const PieceChooserTile = ({ chosenPiece, setPieceSelectorModal }) => {
     setPieceSelectorModal(true);
   };
   return (
-    <div className="w-1/6 h-3/4 rounded-half border-2 border-black bg-white ml-2 flex justify-center items-center">
+    <div className="w-1/6 h-3/4 rounded-half border-2 border-black bg-white ml-2 flex justify-center items-center hover:scale-110 active:scale-110 transition-transform cursor-pointer select-none">
       <Image src={chosenPiece} onClick={handleOnclick}></Image>
     </div>
   );
