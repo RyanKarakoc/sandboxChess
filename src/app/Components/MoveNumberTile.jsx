9b097d3c360aa20496cc1772fea346ab8015b927
RyanKarakoc <kochy@hotmail.co.uk>
2024-02-19 07:02:34 +0000

    moved files into correct folders

--- src/app/Components/MoveNumberTile.jsx
+++
@@ -1,9 +0,0 @@
-const MoveNumberTile = ({ moveNumber }) => {
-  return (
-    <div className="w-1/6 h-3/4 rounded-half border-2 border-black shadow-lg bg-white ml-2 flex justify-center items-center">
-      {moveNumber}
-    </div>
-  );
-};
-
-export default MoveNumberTile;
