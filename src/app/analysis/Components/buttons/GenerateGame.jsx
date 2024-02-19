38a799f5c84626788619c8688c7782a5ca0e840e
RyanKarakoc <kochy@hotmail.co.uk>
2024-02-19 10:21:35 +0000

    created a generate game button

---
+++ src/app/analysis/Components/buttons/GenerateGame.jsx
@@ -0,0 +1,15 @@
+const GenerateGame = ({ setShowboard }) => {
+  const handleOnClick = () => {
+    setShowboard(true);
+  };
+  return (
+    <div
+      onClick={handleOnClick}
+      className="w-1/4 h-auto rounded-half border-2 border-black bg-green-300 shadow-lg p-4 flex justify-center items-center hover:scale-110 active:scale-90 transition-transform select-none cursor-pointer"
+    >
+      Generate Game
+    </div>
+  );
+};
+
+export default GenerateGame;
