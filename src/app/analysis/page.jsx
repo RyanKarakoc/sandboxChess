d987335ab9797729d94b9b27a7cb9a94b4cb3da2
RyanKarakoc <kochy@hotmail.co.uk>
2024-02-28 14:24:15 +0800

    refactored and removed uneeded state

--- src/app/analysis/page.jsx
+++ src/app/analysis/page.jsx
@@ -17,6 +17,11 @@ import Board from "../Components/board";
 import GenerateGame from "./Components/buttons/GenerateGame";
 import MoveList from "../Components/MoveList";
 import Playbar from "./Components/Playbar/Playbar";
+import {
+  GlobalStateProvider,
+  useGlobalState,
+} from "../Components/context/GlobabStateProvider";
+import MovesArea from "./Components/tiles/MovesArea";
 
 export default function Page() {
   const [count, setCount] = useState(0);
@@ -26,7 +31,6 @@ export default function Page() {
   const [chosenEndTile, setChosenEndTile] = useState("a1");
   const [startTileSelectorModal, setStartTileSelectorModal] = useState(false);
   const [chosenStartTile, setChosenStartTile] = useState("a1");
-  const [moves, setMoves] = useState([]);
   const [board, setBoard] = useState([
     ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"],
     ["♟︎", "♟︎", "♟︎", "♟︎", "♟︎", "♟︎", "♟︎", "♟︎"],
@@ -39,7 +43,6 @@ export default function Page() {
   ]);
   const [generateAnyalysisGame, setGenerateAnalysisGame] = useState(false);
   const [showBoard, setShowboard] = useState(false);
-  const [analysisMoveNumber, setAnalysisMoveNumber] = useState(moves.length);
 
   // Create a ref for the scrollable div
   const scrollableDivRef = useRef(null);
@@ -59,116 +62,90 @@ export default function Page() {
       setChosenPiece(whitePawn);
     }
     scrollToBottom();
-  }, [moves]);
+  }, []);
 
   return (
-    <>
-      <Header />
+    <GlobalStateProvider>
+      <>
+        <Header />
 
-      {startTileSelectorModal ? (
-        <StartTileSelectorModal
-          setChosenStartTile={setChosenStartTile}
-          setStartTileSelectorModal={setStartTileSelectorModal}
-        />
-      ) : null}
-      {endTileSelectorModal ? (
-        <EndTileSelectorModal
-          setChosenEndTile={setChosenEndTile}
-          setEndTileSelectorModal={setEndTileSelectorModal}
-        />
-      ) : null}
-      {pieceSelectorModal ? (
-        <PieceSelectorModal
-          moveNumber={count}
-          setChosenPiece={setChosenPiece}
-          setPieceSelectorModal={setPieceSelectorModal}
-        />
-      ) : null}
-      {showBoard ? (
-        <div className="flex justify-center">
-          <Board
-            moves={moves}
-            setMoves={setMoves}
-            generateAnyalysisGame={generateAnyalysisGame}
-            analysisMoveNumber={analysisMoveNumber}
+        {startTileSelectorModal ? (
+          <StartTileSelectorModal
+            setChosenStartTile={setChosenStartTile}
+            setStartTileSelectorModal={setStartTileSelectorModal}
           />
-          <div className="mx-10">
-            <MoveList moves={moves} setMoves={setMoves} />
-            <Playbar
-              analysisMoveNumber={analysisMoveNumber}
-              setAnalysisMoveNumber={setAnalysisMoveNumber}
-              moves={moves}
-            />
+        ) : null}
+        {endTileSelectorModal ? (
+          <EndTileSelectorModal
+            setChosenEndTile={setChosenEndTile}
+            setEndTileSelectorModal={setEndTileSelectorModal}
+          />
+        ) : null}
+        {pieceSelectorModal ? (
+          <PieceSelectorModal
+            moveNumber={count}
+            setChosenPiece={setChosenPiece}
+            setPieceSelectorModal={setPieceSelectorModal}
+          />
+        ) : null}
+        {showBoard ? (
+          <div className="flex justify-center">
+            <Board generateAnyalysisGame={generateAnyalysisGame} />
+            <div className="mx-10">
+              <MoveList />
+              <Playbar />
+            </div>
           </div>
-        </div>
-      ) : (
-        <>
-          <div className="flex flex-col items-center">
-            <h1> What colour are you?</h1>
-            <ColourChooseButton />
-            <h1 className="mt-10 mb-4">Enter game moves.</h1>
-            <div className="w-1/3 h-64 rounded-lg border-2 border-black bg-white flex flex-col items-center">
-              <div className="w-4/5 h-1/3 rounded-half border-2 border-black bg-amber-50 shadow-lg mt-8 flex justify-center items-center">
-                <AlternatingColourTile
-                  colour={count % 2 === 0 ? "White" : "Black"}
-                />
-                <PieceChooserTile
-                  chosenPiece={chosenPiece}
-                  setPieceSelectorModal={setPieceSelectorModal}
-                />
-                <StartTileSelectorTile
-                  chosenStartTile={chosenStartTile}
-                  setStartTileSelectorModal={setStartTileSelectorModal}
-                />
-                <EndTileSelectorTile
-                  chosenEndTile={chosenEndTile}
-                  setEndTileSelectorModal={setEndTileSelectorModal}
-                />
-                <AddMoveButton
-                  moves={moves}
-                  setMoves={setMoves}
-                  chosenPiece={chosenPiece}
-                  chosenStartTile={chosenStartTile}
-                  chosenEndTile={chosenEndTile}
-                  setCount={setCount}
-                  count={count}
-                  board={board}
-                  setBoard={setBoard}
-                />
-              </div>
-              <div
-                ref={scrollableDivRef}
-                className="w-4/5 h-1/3 rounded-half border-2 border-black bg-amber-50 mt-5 overflow-y-auto "
-              >
-                <div className="m-2 flex flex-wrap justify-center items-center">
-                  {moves.map((move, index) => {
-                    const whiteMoveStyle =
-                      "border rounded-md border-black bg-white px-2";
-                    const blackMoveStyle =
-                      "border rounded-md border-black bg-neutral-400 px-2";
-                    return (
-                      <div
-                        key={index}
-                        className={
-                          index % 2 === 0 ? whiteMoveStyle : blackMoveStyle
-                        }
-                      >
-                        {move}
-                      </div>
-                    );
-                  })}
+        ) : (
+          <>
+            <div className="flex flex-col items-center">
+              <h1> What colour are you?</h1>
+              <ColourChooseButton />
+              <h1 className="mt-10 mb-4">Enter game moves.</h1>
+              <div className="w-1/3 h-64 rounded-lg border-2 border-black bg-white flex flex-col items-center">
+                <div className="w-4/5 h-1/3 rounded-half border-2 border-black bg-amber-50 shadow-lg mt-8 flex justify-center items-center">
+                  <AlternatingColourTile
+                    colour={count % 2 === 0 ? "White" : "Black"}
+                  />
+                  <PieceChooserTile
+                    chosenPiece={chosenPiece}
+                    setPieceSelectorModal={setPieceSelectorModal}
+                  />
+                  <StartTileSelectorTile
+                    chosenStartTile={chosenStartTile}
+                    setStartTileSelectorModal={setStartTileSelectorModal}
+                  />
+                  <EndTileSelectorTile
+                    chosenEndTile={chosenEndTile}
+                    setEndTileSelectorModal={setEndTileSelectorModal}
+                  />
+                  <AddMoveButton
+                    chosenPiece={chosenPiece}
+                    chosenStartTile={chosenStartTile}
+                    chosenEndTile={chosenEndTile}
+                    setCount={setCount}
+                    count={count}
+                    board={board}
+                    setBoard={setBoard}
+                  />
+                </div>
+                <div
+                  ref={scrollableDivRef}
+                  className="w-4/5 h-1/3 rounded-half border-2 border-black bg-amber-50 mt-5 overflow-y-auto "
+                >
+                  <MovesArea />
                 </div>
               </div>
             </div>
-          </div>
-          <div className="flex flex-col items-center mt-10">
-            <GenerateGame
-              setShowboard={setShowboard}
-              setGenerateAnalysisGame={setGenerateAnalysisGame}
-            />
-          </div>
-        </>
-      )}
-    </>
+            <div className="flex flex-col items-center mt-10">
+              <GenerateGame
+                setShowboard={setShowboard}
+                setGenerateAnalysisGame={setGenerateAnalysisGame}
+              />
+            </div>
+          </>
+        )}
+      </>
+    </GlobalStateProvider>
   );
 }
