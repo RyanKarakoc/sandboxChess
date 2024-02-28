41f1f26fbd5a79da8747daa00652dbac099631ec
RyanKarakoc <kochy@hotmail.co.uk>
2024-02-28 14:25:05 +0800

    wrapped sandbox page with globalStateProvider

--- src/app/sandbox/page.jsx
+++ src/app/sandbox/page.jsx
@@ -3,19 +3,22 @@ import { useState } from "react";
 import Header from "../Components/Header";
 import MoveList from "../Components/MoveList";
 import Board from "../Components/board";
+import { GlobalStateProvider } from "../Components/context/GlobabStateProvider";
 
 export default function Page() {
   const [moves, setMoves] = useState([]);
 
   return (
-    <>
-      <Header />
-      <div className="flex justify-center ">
-        <Board moves={moves} setMoves={setMoves} />
-        <div className="mx-10 ">
-          <MoveList moves={moves} setMoves={setMoves} />
+    <GlobalStateProvider>
+      <>
+        <Header />
+        <div className="flex justify-center ">
+          <Board moves={moves} setMoves={setMoves} />
+          <div className="mx-10 ">
+            <MoveList moves={moves} setMoves={setMoves} />
+          </div>
         </div>
-      </div>
-    </>
+      </>
+    </GlobalStateProvider>
   );
 }
