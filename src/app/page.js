e1df2c8b61d8490a0f44761872879f1ea291a4b6
RyanKarakoc <kochy@hotmail.co.uk>
2024-02-28 14:23:50 +0800

    wrapped app with globalStateProvider

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
-'use client'
-import Menu from "./Components/Button";
+"use client";
+import MainMenu from "./Components/MainMenu";
 import Header from "./Components/Header";
-
+import { GlobalStateProvider } from "./Components/context/GlobabStateProvider";
 
 export default function home() {
-  return(
-  <>
-    <Header />
-    <Menu />
-  </>
-  )
+  return (
+    <GlobalStateProvider>
+      <Header />
+      <MainMenu />
+    </GlobalStateProvider>
+  );
 }
