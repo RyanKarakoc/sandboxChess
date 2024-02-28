3f9d9704c7c5c18e00e38cfba7e58060da668b89
RyanKarakoc <kochy@hotmail.co.uk>
2024-02-28 14:25:20 +0800

    removed un-needed state

--- src/app/analysis/Components/Playbar/Playbar.jsx
+++ src/app/analysis/Components/Playbar/Playbar.jsx
@@ -1,31 +1,18 @@
+import { useGlobalState } from "@/app/Components/context/GlobabStateProvider";
 import FirstMoveButton from "./FirstMoveButton";
 import LastMoveButton from "./LastMoveButton";
 import NextMoveButton from "./NextMoveButton";
 import PreviousMoveButton from "./PreviousMoveButton";
 
-const Playbar = ({ analysisMoveNumber, setAnalysisMoveNumber, moves }) => {
+const Playbar = () => {
+  const { alternateMove, setAlternateMove, moves, setMoves } = useGlobalState();
+
   return (
     <div className="w-auto h-20 rounded-md border-4 border-black bg-amber-200 flex items-center justify-center">
-      <FirstMoveButton
-        analysisMoveNumber={analysisMoveNumber}
-        setAnalysisMoveNumber={setAnalysisMoveNumber}
-        moves={moves}
-      />
-      <PreviousMoveButton
-        analysisMoveNumber={analysisMoveNumber}
-        setAnalysisMoveNumber={setAnalysisMoveNumber}
-        moves={moves}
-      />
-      <NextMoveButton
-        analysisMoveNumber={analysisMoveNumber}
-        setAnalysisMoveNumber={setAnalysisMoveNumber}
-        moves={moves}
-      />
-      <LastMoveButton
-        analysisMoveNumber={analysisMoveNumber}
-        setAnalysisMoveNumber={setAnalysisMoveNumber}
-        moves={moves}
-      />
+      <FirstMoveButton />
+      <PreviousMoveButton />
+      <NextMoveButton />
+      <LastMoveButton />
     </div>
   );
 };
